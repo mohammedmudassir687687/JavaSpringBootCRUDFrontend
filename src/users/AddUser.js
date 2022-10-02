e0ca2028ae600eef9ab94aedc3c141ad550a41e8
Mohammed Mudassir <mohammedmudassir687687@gmail.com>
2022-10-02 13:59:25 -0400

    Created Register User Form

--- src/users/AddUser.js
+++ src/users/AddUser.js
@@ -2,7 +2,43 @@ import React from 'react'
 
 function AddUser() {
   return (
-    <div>AddUser Form</div>
+    <div className='container'>
+        <div className='row'>
+            <div className='col-md-6 offset-md-3 border rounded p-4 mt-2 shadow'>
+                <h2 className='text-center m-4'>Register User</h2>
+                <div className='mb-3'>
+                    <label htmlFor='Name' className='form-label'>Name</label>
+                    <input
+                        type={'text'}
+                        className='form-control'
+                        placeholder='Enter your name'
+                        name='name'
+                    />
+                </div>
+                <div className='mb-3'>
+                    <label htmlFor='username' className='form-label'>Username</label>
+                    <input
+                        type={'text'}
+                        className='form-control'
+                        placeholder='Enter your name'
+                        name='username'
+                    />
+                </div>
+                <div className='mb-3'>
+                    <label htmlFor='email' className='form-label'>Email</label>
+                    <input
+                        type={'email'}
+                        className='form-control'
+                        placeholder='Enter your name'
+                        name='email'
+                    />
+                </div>
+                <button type='submit' className='btn btn-outline-primary'>Submit</button>
+                <button type='submit' className='btn btn-outline-danger mx-2'>Cancel</button>
+
+            </div>
+        </div>
+    </div>
   )
 }
 
