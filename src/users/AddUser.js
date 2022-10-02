3aa36470b437e7de3033988ada6f6895b4c6fec0
Mohammed Mudassir <mohammedmudassir687687@gmail.com>
2022-10-02 14:14:38 -0400

    using axios to post data in database

--- src/users/AddUser.js
+++ src/users/AddUser.js
@@ -1,6 +1,10 @@
+import axios from 'axios';
 import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
 
 function AddUser() {
+
+    let navigate = useNavigate();
 
     const [user, setUser] = useState({
         name:'',
@@ -14,47 +18,55 @@ function AddUser() {
         setUser({...user, [e.target.name]: e.target.value})
     }
 
+    const onSubmit= async(e) => {
+        e.preventDefault();
+        await axios.post('http://localhost:8080/user', user);
+        navigate('/');
+    };
+
   return (
     <div className='container'>
         <div className='row'>
             <div className='col-md-6 offset-md-3 border rounded p-4 mt-2 shadow'>
                 <h2 className='text-center m-4'>Register User</h2>
-                <div className='mb-3'>
-                    <label htmlFor='Name' className='form-label'>Name</label>
-                    <input
-                        type={'text'}
-                        className='form-control'
-                        placeholder='Enter your name'
-                        name='name'
-                        value={name}
-                        onChange={(e)=>onInputChange(e)}
-                    />
-                </div>
-                <div className='mb-3'>
-                    <label htmlFor='username' className='form-label'>Username</label>
-                    <input
-                        type={'text'}
-                        className='form-control'
-                        placeholder='Enter your name'
-                        name='username'
-                        value={username}
-                        onChange={(e)=>onInputChange(e)}
-                    />
-                </div>
-                <div className='mb-3'>
-                    <label htmlFor='email' className='form-label'>Email</label>
-                    <input
-                        type={'email'}
-                        className='form-control'
-                        placeholder='Enter your name'
-                        name='email'
-                        value={email}
-                        onChange={(e)=>onInputChange(e)}
-                    />
-                </div>
-                <button type='submit' className='btn btn-outline-primary'>Submit</button>
-                <button type='submit' className='btn btn-outline-danger mx-2'>Cancel</button>
+                <form onSubmit={(e)=>onSubmit(e)}>
+                    <div className='mb-3'>
+                        <label htmlFor='Name' className='form-label'>Name</label>
+                        <input
+                            type={'text'}
+                            className='form-control'
+                            placeholder='Enter your name'
+                            name='name'
+                            value={name}
+                            onChange={(e)=>onInputChange(e)}
+                        />
+                    </div>
+                    <div className='mb-3'>
+                        <label htmlFor='username' className='form-label'>Username</label>
+                        <input
+                            type={'text'}
+                            className='form-control'
+                            placeholder='Enter your name'
+                            name='username'
+                            value={username}
+                            onChange={(e)=>onInputChange(e)}
+                        />
+                    </div>
+                    <div className='mb-3'>
+                        <label htmlFor='email' className='form-label'>Email</label>
+                        <input
+                            type={'email'}
+                            className='form-control'
+                            placeholder='Enter your name'
+                            name='email'
+                            value={email}
+                            onChange={(e)=>onInputChange(e)}
+                        />
+                    </div>
+                    <button type='submit' className='btn btn-outline-primary'>Submit</button>
+                    <Link className='btn btn-outline-danger mx-2' to='/'>Cancel</Link>
 
+                </form>
             </div>
         </div>
     </div>
